3c08856670f6d0857f25bcd686772bcf13a31cd1
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-12-09 13:56:03 +0530

    all set

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
   const addToComplete = (task) => {
     setCompletedTask((preState) => [...preState, task])
   }
-  
+
   const deleteCompleteToDo = (id) => {
     const newCompletedTasks = completedTask.filter(t => t.id!== id)
     setCompletedTask(newCompletedTasks)
@@ -32,19 +32,20 @@ const App = () => {
 
   return (
     <div className='mx-auto max-w-screen-xl w-full min-h-screen bg-[url(../src/assets/bg-img.png)] bg-[60%_90%] bg-no-repeat'>
+      <h1 className='text-3xl font-bold font-serif ml-2'>My To Do List</h1> 
       <TaskInput setToDo={setToDo} />
       <div className='flex justify-around flex-col md:flex-row w-[80%] mt-5 mx-auto gap-y-5'>
-        <div className='bg-orange-500/60 max-w-80 w-full min-h-64'>
-          <h3 className='text-center mb-3 font-mono'>Your Task</h3>
-          <ul className='p-2 flex flex-col gap-2'>
+        <div className='bg-orange-500/30 max-w-80 w-full min-h-64 backdrop-blur-sm'>
+          <h3 className='text-center m-1 font-semibold font-serif text-lg '>Your Task</h3>
+          <ul className='p-2 flex flex-col gap-2 overflow-y-scroll max-h-96'>
             {tasks.map((task) => (
               <PendingTaskList addToComplete={addToComplete} task={task} key={task.id} deleteToDo={deleteToDo} />
             ))}
           </ul>
         </div>
-        <div className='bg-green-500/60 max-w-80 w-full min-h-64'>
-          <h3 className='text-center mb-3 font-mono'>Completed Task</h3>
-          <ul>
+        <div className='bg-green-500/20 backdrop-blur-sm max-w-80 w-full min-h-64'>
+          <h3 className='text-center m-1 font-semibold font-serif text-lg'>Completed Task</h3>
+          <ul className='overflow-y-scroll max-h-96'>
             {completedTask.map((task) => (
               <CompletedTaskList deleteToDo={deleteCompleteToDo} completedTask={task} />
             ))
